9b479b86d2dacf7f5a36ed559ea14148b94db7ff
vileroze <50352881+vileroze@users.noreply.github.com>
2021-12-13 17:57:35 +0545

    Added an interface 'ShapeInterface' to set the params of a shape and also to draw the actual graphics, and an abstract class 'Shape' that inherits from the before mentioned Interface

---
+++ GraphicalProgrammingLanguage/GraphicalProgrammingLanguage/Shape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalProgrammingLanguage
+{
+    abstract class Shape : ShapeInterface
+    {
+        protected Color colour; //shape's colour
+        protected int x, y; //not I could use c# properties for this
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(Color colour, int x, int y)
+        {
+            this.colour = colour; //shape's colour
+            this.x = x; //its x pos
+            this.y = y; //its y pos
+        }
+
+        public abstract void draw(Graphics g);
+
+        public virtual void set(Color c, params int[] list)
+        {
+            this.colour = c;
+            this.x = list[0];
+            this.y = list[1];
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "    " + this.x + "," + this.y + " : ";
+        }
+    }
+}
---
+++ GraphicalProgrammingLanguage/GraphicalProgrammingLanguage/ShapeInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalProgrammingLanguage
+{
+    interface ShapeInterface
+    {
+        void set(Color c, params int[] list); // no need to declare the size of array if "params" is used
+        void draw(Graphics g);
+    }
+}
